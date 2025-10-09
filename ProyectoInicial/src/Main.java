cedf89fbf6f0c553b570f398d464f416e0215645
josejuanruizmunoz <jjruizmunoz98@gmail.com>
2025-10-09 19:16:37 +0200

    update

--- ProyectoInicial/src/Main.java
+++ ProyectoInicial/src/Main.java
@@ -1,2 +1,24 @@
 public class Main {
+
+    // esto es un comentario
+    //otra linea de comentario
+    /*este comentario
+    se puede dividir
+    en varias lineas de codigo
+     */
+    // TODO me falta este
+
+    public static void main(String[] args){
+      System.out.println("Este es mi promer programa en Java\n");
+      System.out.println("Esta es la segunda linea del programa");
+
+  }
+
+
+  public void nombreMetodo(){
+
+    }
+
+    // TODO esto tambien esta pendiente
+
 }
