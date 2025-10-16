526b4698edae60452be64d00251cdb89209d7696
josejuanruizmunoz <jjruizmunoz98@gmail.com>
2025-10-16 19:24:31 +0200

    Update Main.java

--- ProyectoInicial/src/Main.java
+++ ProyectoInicial/src/Main.java
@@ -10,9 +10,38 @@ public class Main {
 
     public static void main(String[] args){
       System.out.println("Este es mi promer programa en Java\n");
-      System.out.println("Esta es la segunda linea del programa");
+      System.out.println("Esta es la segunda linea del programa\n");
+
+      String miNombre = "Borja"; // null
+      char letraDNI = 'A';
+      int edad = 41;
+      double altura = 1.71;
+      float peso = 78.7f;
+      boolean experiencia = true;
+
+      miNombre = "Borja M. H.";
+      letraDNI = 'B';
+      altura =  1.75;
+      peso = 67.5f;
+      experiencia = false;
+
+      System.out.println("Mi nombre es: "+miNombre );
+      System.out.println("La letra de mi DNI es: "+letraDNI);
+      System.out.println("Mi edad es: " +edad+ " años");
+      System.out.println("Mi altura es: " +altura+ " metros");
+      System.out.println("Mi peso es: " +peso+ " kilogramos");
+      System.out.println("Mi experiencia es: " +experiencia);
+
+
 
   }
+
+
+
+
+
+
+
 
 
   public void nombreMetodo(){
